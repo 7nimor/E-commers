30aecf1b7a1bae16b8dd67dede5b1c2bf273516f
7nimor <nima7playstation4@gmail.com>
2023-11-01 22:38:35 +0330

    create models categories and products

--- utils.py
+++ utils.py
@@ -2,6 +2,17 @@ from kavenegar import *
 
 
 def send_otp_code(phone_number,code):
-    api = KavenegarAPI('554B5A4138556E577530697257525246757A48314B7956495131396752684A54382B6F455361335666576B3D')
-    params = {f'sender': '1000689696', 'receptor': {phone_number}, 'message': f'برگام جوجو کار کرد{code}'}
-    response = api.sms_send(params)
+    try:
+        api = KavenegarAPI('554B5A4138556E577530697257525246757A48314B7956495131396752684A54382B6F455361335666576B3D')
+        params = {
+            'sender': '',  # optional
+            'receptor': '09165597588',  # multiple mobile number, split by comma
+            'message': code,
+        }
+        response = api.sms_send(params)
+        print(response)
+    except APIException as e:
+        print(e)
+    except HTTPException as e:
+        print(e)
+
