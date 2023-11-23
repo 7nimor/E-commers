0f395887772e22f9aced4a5a37a07b8c3f6e28c1
7nimor <mima7playstation4@gmail.com>
2023-11-23 22:26:56 +0330

    create categoty

--- home/urls.py
+++ home/urls.py
@@ -1,18 +1,19 @@
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 
 app_name = 'home'
 
 bucket_urls = [
     path('', views.BucketView.as_view(), name='bucket'),
-    path('delete_obj/<str:key>',views.DeleteObjectBucket.as_view(), name='delete_obj_bucket'),
-    path('download_obj/<str:key>',views.DownloadObjectBucket.as_view(), name='download_obj_bucket'),
-    path('upload_obj/',views.UploadObjectBucket.as_view(), name='upload_obj_bucket'),
+    path('delete_obj/<str:key>', views.DeleteObjectBucket.as_view(), name='delete_obj_bucket'),
+    path('download_obj/<str:key>', views.DownloadObjectBucket.as_view(), name='download_obj_bucket'),
+    path('upload_obj/', views.UploadObjectBucket.as_view(), name='upload_obj_bucket'),
 
 ]
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('bucket/',include(bucket_urls)),
+    path('category/<slug:category_slug>/', views.HomeView.as_view(), name='category_filter'),
+    path('bucket/', include(bucket_urls)),
     path('<slug:slug>/', views.ProductsDetailView.as_view(), name='product_detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,16 +3,20 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 
 from utils import IsAdminMixin
-from .models import Product
+from .models import Product, Category
 from . import tasks
 from . import forms
 
 
 # Create your views here.
 class HomeView(View):
-    def get(self, request):
+    def get(self, request, category_slug=None):
         products = Product.objects.filter(available=True)
-        return render(request, 'home/home.html', {'products': products})
+        categories = Category.objects.all()
+        if category_slug:
+            category=Category.objects.get(slug=category_slug)
+            products = products.filter(category=category)
+        return render(request, 'home/home.html', {'products': products,'categories': categories})
 
 
 class ProductsDetailView(View):
@@ -21,21 +25,21 @@ class ProductsDetailView(View):
         return render(request, 'home/details.html', {'product': product})
 
 
-class BucketView(IsAdminMixin,View):
+class BucketView(IsAdminMixin, View):
     def get(self, request):
         form = forms.UploadObjBucket()
         objects = tasks.all_bucket_object_task()
-        return render(request, 'home/bucket.html', {'objects': objects,'form':form})
+        return render(request, 'home/bucket.html', {'objects': objects, 'form': form})
 
 
-class DeleteObjectBucket(IsAdminMixin,View):
+class DeleteObjectBucket(IsAdminMixin, View):
     def get(self, request, key):
         tasks.delete_bucket_object_task.delay(key)
         messages.success(request, 'your image will be delete', 'info')
         return redirect('home:bucket')
 
 
-class DownloadObjectBucket(IsAdminMixin,View):
+class DownloadObjectBucket(IsAdminMixin, View):
     def get(self, request, key):
         tasks.download_bucket_object_task.delay(key)
         messages.success(request, 'your file will be downloaded', 'info')
